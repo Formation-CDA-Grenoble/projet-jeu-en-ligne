edaca88cdf2cbfb1db856043ad5fd05f388a8a2a
Vincent <vincent.grange38@gmail.com>
2020-02-19 11:01:13 +0100

    game.module

--- back/src/games/games.module.ts
+++ back/src/games/games.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GamesService } from './games.service';
 import { GamesController } from './games.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Game } from './game.entity';
 
 @Module({
-  providers: [GamesService],
-  controllers: [GamesController]
+	imports: [TypeOrmModule.forFeature([Game])],
+  	providers: [GamesService],
+  	controllers: [GamesController]
 })
 export class GamesModule {}
