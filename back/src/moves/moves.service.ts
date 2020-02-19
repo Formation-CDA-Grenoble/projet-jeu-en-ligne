58c28444da2bf6e8b89d5a2a288025f7f963c896
Vincent <vincent.grange38@gmail.com>
2020-02-19 11:57:39 +0100

    moves.service

--- back/src/moves/moves.service.ts
+++ back/src/moves/moves.service.ts
@@ -1,4 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Move } from './move.entity';
 
 @Injectable()
-export class MovesService {}
+export class MovesService {
+    constructor(
+        @InjectRepository(Move)
+        private readonly movesRepository: Repository<Move>,
+    ) {}
+
+    async getAllmoves(): Promise<Move[]> {
+    	const moves = await this.movesRepository.find();
+    	return moves;
+    }
+
+    async getAllMovesByGame(gameID:number): Promise<Move[]> {
+    	const moves = await this.movesRepository.find({ where: { game: gameID }});
+        return moves;
+    }
+
+    async getAllMovesByUser(userID:number): Promise<Move[]> {
+    	const moves = await this.movesRepository.find({ where: { user: userID }});
+        return moves;
+    }
+
+    async getAllMovesByUserByGame(gameID:number, userID:number): Promise<Move[]> {
+    	const moves = await this.movesRepository.find({ where: { game: gameID, player: userID }});
+        return moves;
+    }
+
+    async addMove(entityMove: Move): Promise<Move> {
+    	const move = await this.movesRepository.save(entityMove);
+        return move;   
+    }
+
+    async updateMove(entityMove: Move): Promise<Move> {
+    	const move = await this.movesRepository.save(entityMove);
+    	return move;
+    }
+
+    async deleteMove(moveID): Promise<any> {
+        let moveToDeleted = await this.movesRepository.findOne(moveID);
+        const deletedMove = await this.movesRepository.remove(moveToDeleted);
+        return deletedMove;
+    }
+}
