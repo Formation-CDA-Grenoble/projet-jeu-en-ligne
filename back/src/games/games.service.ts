583566e6d0bbd92a33194335deb64caea63755a6
Vincent <vincent.grange38@gmail.com>
2020-02-19 11:21:02 +0100

    games.service

--- back/src/games/games.service.ts
+++ back/src/games/games.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Game } from './game.entity';
 
 @Injectable()
-export class GamesService {}
+export class GamesService {
+    constructor(
+        @InjectRepository(Game)
+        private readonly gamesRepository: Repository<Game>,
+    ) {}
+
+    async getAllgames(): Promise<Game[]> {
+    	const games = await this.gamesRepository.find({ relations: ["player1", "player2"] });
+    	return games;
+    }
+
+    async getGame(gameID:number): Promise<Game> {
+    	const game = await this.gamesRepository.findOne(gameID, { relations: ["player1", "player2"] });
+        return game;
+    }
+
+    async addGame(entityGame: Game): Promise<Game> {
+    	const game = await this.gamesRepository.save(entityGame);
+        return game;   
+    }
+
+    async updateGame(entityGame: Game): Promise<Game> {
+    	const game = await this.gamesRepository.save(entityGame);
+    	return game;
+    }
+
+    async deleteGame(gameID): Promise<any> {
+        let gameToDeleted = await this.gamesRepository.findOne(gameID);
+        const deletedGame = await this.gamesRepository.remove(gameToDeleted);
+        return deletedGame;
+    }
+}
